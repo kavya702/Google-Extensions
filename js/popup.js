fd4529842a7b86e4c9c24679a71fe0775d0fb070
kavya702 <34915090+kavya702@users.noreply.github.com>
2017-12-30 12:01:07 +0530

    Extension for chrome  for sms

---
+++ js/popup.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    var checkPageClick = document.getElementById("checkpage");
+    checkPageClick.addEventListener("click",function () {
+        alert("Hello World");
+    })
+
+});
